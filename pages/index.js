63549518532c11b24d5665dc946c87ba882863cf
Shivnath Kisku <124551959+Shivkisku@users.noreply.github.com>
2024-08-29 20:40:34 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Loader from '../components/Loader';
+
+export default function Home() {
+  return (
+    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+      <Loader />
+    </div>
+  );
+}
